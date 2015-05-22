31b1889177260cdf5a7bd03a544a7bd902dee0c0
pacocampo <paco.ocampor@gmail.com>
2015-05-22 16:19:19 -0500

    Interface design and Facebook login added

---
+++ FastMarkett/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  FastMarkett
+//
+//  Created by Francisco Ocampo Romero on 22/05/15.
+//  Copyright (c) 2015 Nicholas Raphael Farias . All rights reserved.
+//
+
+#ifndef FastMarkett_Bridging_Header_h
+#define FastMarkett_Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif
---
+++ FastMarkett/FacebookViewController.swift
@@ -0,0 +1,97 @@
+//
+//  FacebookViewController.swift
+//  FastMarkett
+//
+//  Created by Francisco Ocampo Romero on 22/05/15.
+//  Copyright (c) 2015 Nicholas Raphael Farias . All rights reserved.
+//
+
+import UIKit
+
+class FacebookViewController: UIViewController, FBSDKLoginButtonDelegate {
+    
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        if (FBSDKAccessToken.currentAccessToken() != nil)
+        {
+            // User is already logged in, do work such as go to next view controller.
+            self.performSegueWithIdentifier("segueLista", sender: nil)
+        }
+        else
+        {
+            let loginView : FBSDKLoginButton = FBSDKLoginButton()
+            self.view.addSubview(loginView)
+            loginView.center = self.view.center
+            loginView.readPermissions = ["public_profile", "email", "user_friends"]
+            loginView.delegate = self
+        }
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // Facebook Delegate Methods
+
+    
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        println("User Logged In")
+        
+        if ((error) != nil)
+        {
+            // Process error
+        }
+        else if result.isCancelled {
+            // Handle cancellations
+        }
+        else {
+            // If you ask for multiple permissions at once, you
+            // should check if specific permissions missing
+            if result.grantedPermissions.contains("email")
+            {
+                // Do work
+                self.performSegueWithIdentifier("segueLista", sender: nil)
+            }
+        }
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        println("User Logged Out")
+    }
+    
+    func returnUserData()
+    {
+        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: nil)
+        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+            
+            if ((error) != nil)
+            {
+                // Process error
+                println("Error: \(error)")
+            }
+            else
+            {
+                println("fetched user: \(result)")
+                let userName : NSString = result.valueForKey("name") as! NSString
+                println("User Name is: \(userName)")
+                let userEmail : NSString = result.valueForKey("email") as! NSString
+                println("User Email is: \(userEmail)")
+            }
+        })
+    }
+    
+}
+
+
